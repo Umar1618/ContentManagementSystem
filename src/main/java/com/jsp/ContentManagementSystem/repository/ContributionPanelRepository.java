3b7c35fe7dd40e9e021bb905ce84132c4d1298df
Umar Farook <ufarook9677@gmail.com>
2024-04-02 10:20:45 +0530

    user validation

--- src/main/java/com/jsp/ContentManagementSystem/repository/ContributionPanelRepository.java
+++ src/main/java/com/jsp/ContentManagementSystem/repository/ContributionPanelRepository.java
@@ -3,7 +3,10 @@ package com.jsp.ContentManagementSystem.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jsp.ContentManagementSystem.model.ContributionPanel;
+import com.jsp.ContentManagementSystem.model.User;
 
 public interface ContributionPanelRepository extends JpaRepository<ContributionPanel, Integer> {
+
+	boolean existsByPanelIdAndContributors(int panelId, User user);
 	
 }
--- src/main/java/com/jsp/ContentManagementSystem/serviceimpl/BlogPostServiceImpl.java
+++ src/main/java/com/jsp/ContentManagementSystem/serviceimpl/BlogPostServiceImpl.java
@@ -2,14 +2,19 @@ package com.jsp.ContentManagementSystem.serviceimpl;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.jsp.ContentManagementSystem.enums.PostType;
 import com.jsp.ContentManagementSystem.exception.BlogNotFoundByIdException;
 import com.jsp.ContentManagementSystem.exception.BlogPostNotFoundByIdException;
+import com.jsp.ContentManagementSystem.exception.IllegalAccessRequestException;
+import com.jsp.ContentManagementSystem.model.Blog;
 import com.jsp.ContentManagementSystem.model.BlogPost;
 import com.jsp.ContentManagementSystem.repository.BlogPostRepository;
 import com.jsp.ContentManagementSystem.repository.BlogRepository;
+import com.jsp.ContentManagementSystem.repository.ContributionPanelRepository;
+import com.jsp.ContentManagementSystem.repository.UserRepository;
 import com.jsp.ContentManagementSystem.requestdto.BlogPostRequest;
 import com.jsp.ContentManagementSystem.responsedto.BlogPostResponse;
 import com.jsp.ContentManagementSystem.service.BlogPostService;
@@ -21,11 +26,16 @@ public class BlogPostServiceImpl implements BlogPostService {
 	private BlogRepository blogRepository;
 	private ResponseStructure<BlogPostResponse> structure;
 	private BlogPostRepository blogPostRepository;
+	private ContributionPanelRepository contributionPanelRepository;
+	private UserRepository userRepository;
 	
-	public BlogPostServiceImpl(BlogRepository blogRepository, ResponseStructure<BlogPostResponse> structure, BlogPostRepository blogPostRepository) {
+	public BlogPostServiceImpl(BlogRepository blogRepository, ResponseStructure<BlogPostResponse> structure, 
+			BlogPostRepository blogPostRepository, ContributionPanelRepository contributionPanelRepository, UserRepository userRepository) {
 		this.blogRepository = blogRepository;
 		this.structure = structure;
 		this.blogPostRepository = blogPostRepository;
+		this.contributionPanelRepository = contributionPanelRepository;
+		this.userRepository = userRepository;
 	}
 	
 	private BlogPostResponse mapToBlogPostResponse(BlogPost blogPost) {
@@ -53,6 +63,8 @@ public class BlogPostServiceImpl implements BlogPostService {
 	@Override
 	public ResponseEntity<ResponseStructure<BlogPostResponse>> createBlogPostDraft(int blogId, BlogPostRequest blogPostRequest) {
 		return blogRepository.findById(blogId).map(blog -> {
+			if(!validateUser(blog))
+				throw new IllegalAccessRequestException("Faild to create blogpost");
 			BlogPost blogPost = mapToBlogPostEntity(blogPostRequest, new BlogPost());
 			blogPost.setBlog(blog);
 			blogPost = blogPostRepository.save(blogPost);
@@ -64,10 +76,20 @@ public class BlogPostServiceImpl implements BlogPostService {
 		}).orElseThrow(()-> new BlogNotFoundByIdException("Faild to create blogpost"));
 	}
 
+	private boolean validateUser(Blog blog) {
+		String email = SecurityContextHolder.getContext().getAuthentication().getName();
+		return userRepository.findByEmail(email).map(user -> 
+			(email.equals(blog.getUser().getEmail()) || contributionPanelRepository
+					.existsByPanelIdAndContributors(blog.getContributionPanel().getPanelId(),user))
+		).orElse(false);
+	}
+	
 	@Override
 	public ResponseEntity<ResponseStructure<BlogPostResponse>> updateBlogPostDraft(int postId,
 			BlogPostRequest blogPostRequest) {
 		return blogPostRepository.findById(postId).map(blogPost -> {
+			if(validateUser(blogPost.getBlog()))
+				throw new IllegalAccessRequestException("Faild to create blogpost");
 			blogPost = blogPostRepository.save(mapToBlogPostEntity(blogPostRequest, blogPost));
 			return ResponseEntity.ok(structure.setStatus(HttpStatus.OK.value())
 					.setMessage("BlogPost updated successfully")
